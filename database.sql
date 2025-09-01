8793e84ba51e8c5f6ec837563a6913ec7e117505
ElMaikina <mikesoto890@gmail.com>
2025-09-01 09:38:48 -0400

    crear src

--- database.sql
+++ database.sql
@@ -360,10 +360,10 @@ Clase que representa a un usuario de MatchaFunding.
 */
 CREATE TABLE Usuario (
 	ID bigint NOT NULL AUTO_INCREMENT,
-	NombreDeUsuario varchar(200) NOT NULL,
+	NombreDeUsuario varchar(200),
 	Contrasena varchar(200) NOT NULL,
 	Correo varchar(200) NOT NULL,
-	Persona bigint NOT NULL,
+	Persona bigint,
 	PRIMARY KEY (ID),
 	FOREIGN KEY (Persona) REFERENCES Persona(ID)
 );
